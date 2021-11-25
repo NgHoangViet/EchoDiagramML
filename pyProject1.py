1abc9f867816ccbe438ac4377969dc36937d251a
unknown <nishino342000@gmail.com>
2021-11-25 20:05:06 +0700

    vgg16

--- pyProject1.py
+++ pyProject1.py
@@ -30,76 +30,6 @@ testing_transforms = transforms.Compose([transforms.Resize((224, 224)),
 
 
 # Need Resize(32,32)
-# class VGG16(pl.LightningModule):
-#     def __init__(self):
-#         super().__init__()
-#         self.features = self._make_layers()
-#
-#         self.classfier_head = nn.Linear(512, 3)
-#         self.accuracy = torchmetrics.Accuracy()
-#
-#     def forward(self, x):
-#         out = self.features(x)
-#         out = self.classfier_head(out.view(out.size(0), -1))
-#         return out
-#
-#     def _make_layers(self):
-#         config = [64, 64, 'MP', 128, 128, 'MP', 256, 256, 256, 'MP', 512, 512, 512, 'MP', 512, 512, 512, 'MP']
-#         layers = []
-#         c_in = 3
-#         for c in config:
-#             if c == 'MP':
-#                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-#             else:
-#                 layers += [
-#                     nn.Conv2d(in_channels=c_in, out_channels=c, kernel_size=3, padding=1),
-#                     nn.BatchNorm2d(c),
-#                     nn.ReLU6(inplace=True)
-#                 ]
-#                 c_in = c
-#         return nn.Sequential(*layers)
-#
-#     def training_step(self, batch, batch_idx):
-#         images, labels = batch
-#
-#         # Forward pass
-#         outputs = self(images)
-#         loss = F.cross_entropy(outputs, labels)
-#
-#         self.log('train_acc', self.accuracy(outputs, labels))
-#         self.log("train_loss", loss)
-#         return loss
-#
-#     def train_dataloader(self):
-#         train_dataset = torchvision.datasets.ImageFolder(train_dir, transform=training_transforms)
-#
-#         train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, num_workers=4,
-#                                                    shuffle=True)
-#         return train_loader
-#
-#     def test_dataloader(self):
-#         test_dataset = torchvision.datasets.ImageFolder(test_dir, transform=testing_transforms)
-#
-#         test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, num_workers=4,
-#                                                   shuffle=False)
-#         return test_loader
-#
-#     def test_step(self, batch, batch_idx):
-#         images, labels = batch
-#
-#         # Forward pass
-#         outputs = self(images)
-#         loss = F.cross_entropy(outputs, labels)
-#
-#         self.log('test_acc', self.accuracy(outputs, labels))
-#         self.log("test_loss", loss)
-#         return loss
-#
-#     def configure_optimizers(self):
-#         return torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
-
-
-# Need Resize(32,32)
 class ConvNet(pl.LightningModule):
     def __init__(self):
         super(ConvNet, self).__init__()
@@ -287,7 +217,6 @@ class VGG16(pl.LightningModule):
         for param in self.features.parameters():
             param.requires_grad = False
         # change the last layer
-
         self.features.classifier[6] = torch.nn.Linear(4096, 3, bias=True)
         self.accuracy = torchmetrics.Accuracy()
 
@@ -333,7 +262,7 @@ class VGG16(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.SGD(self.parameters(), lr=learning_rate)
+        return torch.optim.Adam(self.parameters(), lr=learning_rate)
 
 
 if __name__ == '__main__':
